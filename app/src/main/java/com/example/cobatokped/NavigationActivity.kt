d594db274c70347721214fec7cc33cb162d90b1f
Luzkyman <luqman.afif72@gmail.com>
2023-04-10 23:25:40 +0700

    Atur Login Shared Preference

--- app/src/main/java/com/example/cobatokped/NavigationActivity.kt
+++ app/src/main/java/com/example/cobatokped/NavigationActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cobatokped
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.cobatokped.databinding.ActivityNavigationBinding
+import com.example.cobatokped.util.Prefs
 
 class NavigationActivity : AppCompatActivity() {
 
@@ -26,10 +28,23 @@ class NavigationActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_keranjang))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.setOnItemSelectedListener {
+
+            if (it.itemId == R.id.navigation_notifications){
+                val s = Prefs(this)
+                if(s.getIsLogin()){
+                    Log.d("TAG", "sudah login")
+                } else {
+                    Log.d("TAG", "blum login, pindah ke menu login")
+                }
+            } else {
+                Log.d("TAG", "oncreate: notif lain" + it.itemId)
+            }
+
+            return@setOnItemSelectedListener true
+        }
     }
 }
